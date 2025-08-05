4f442bc55d09b51da9e794f2ba83d2a392f9fa5f
CLEMENT-ANDI EDET <cedet@udallas.edu>
2025-08-04 23:03:40 -0500

    update

--- prompt_loader.py
+++ prompt_loader.py
@@ -19,8 +19,9 @@ class PromptLoader:
     def set_job_description(self, job_description, prompt_name):
         self.load_system_prompt(prompt_name)
         for i, line in enumerate(self.system_prompt):
+            line = line.strip()
             if re.match("The job description you will be working with:", line):
                 self.system_prompt[i] += " " + self.cleaner.remove_new_line_char(job_description)
 
     def set_resume_content(self, resume_content):
-        self.user_prompt = ["Resume"] + list(map(self.cleaner.add_tab_to_start, resume_content.split("\n").copy()))
+        self.user_prompt = list(map(self.cleaner.add_tab_to_start, resume_content.split("\n").copy()))
